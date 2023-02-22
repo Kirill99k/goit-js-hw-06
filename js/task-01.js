2350d1b8816d703d432effae8a97d7665228156f
Kirill Kutovuy <114417403+Kirill99k@users.noreply.github.com>
2023-02-22 18:10:30 +0200

    task-01 done

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+const itemEl = document.querySelectorAll('li.item');
+
+console.log(`Number of categories: ${itemEl.length}`);
+
+
+itemEl.forEach(element => {
+    const titleEl = element.firstElementChild;
+    const innerLiEl = element.lastElementChild.children;
+    console.log('Category: ', titleEl.textContent);
+    console.log('Elements: ', innerLiEl.length);
+})
