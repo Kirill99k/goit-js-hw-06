18ced5b753da978cb0f6e10d7fd118d121ebaa5b
Kirill Kutovuy <114417403+Kirill99k@users.noreply.github.com>
2023-02-27 23:15:18 +0200

    task-10 done

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,40 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  inputCollection: document.querySelector('#controls').firstElementChild,
+  btnCreate: document.querySelector("[data-create]"),
+  btmDestroy: document.querySelector("[data-destroy]"),
+  divBoxes: document.querySelector("#boxes"),
+};
+
+refs.divBoxes.setAttribute(
+  "style",
+  "display: flex; justify-content: center; align-items: center; flex-wrap: wrap; gap: 10px;"
+);
+
+refs.btnCreate.addEventListener("click", () =>
+  createBoxes(refs.inputCollection.value)
+);
+refs.btmDestroy.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  const boxes = [];
+
+  for (let i = 0; i < amount; i += 1) {
+    const size = 50 + i * 10;
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.fontSize = `${size / 4}px`;
+    boxes.push(box);
+  }
+  refs.divBoxes.append(...boxes);
+}
+
+function destroyBoxes() {
+  refs.divBoxes.innerHTML = "";
+}
